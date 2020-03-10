4ac47d06a2c5bf71e5fe8f2e97cffb3180ad0753
Mulugeta Adamu <56386485+muleA@users.noreply.github.com>
2020-03-10 11:20:46 +0300

    Create login.php

---
+++ Admin/login.php
@@ -0,0 +1,149 @@
+<!doctype html>
+
+
+<html>
+
+
+<head>
+
+<title> home</title>
+
+
+<!--  -->
+
+<link rel = "stylesheet" href = "../Style.css">
+
+<meta name = 'viewport' content = 'width=device-width, initial-scale=1'>
+
+<link rel = 'stylesheet' href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/
+    css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel = 'stylesheet' href = 'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+<!-- jQuery library -->
+<script src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js'></script>
+
+<!-- Latest compiled JavaScript -->
+<script src = 'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js'></script>
+</head>
+
+
+
+<body style=" background-color: aliceblue;" >
+
+
+<!--  navigation bar -->
+
+
+<nav class = 'navbar navbar-inverse navbar-static-top'>
+<div class = 'container-fluid'>
+<div class = 'navbar-header'>
+<button type = 'button' class = 'navbar-toggle' data-toggle = 'collapse' data-target = '#myNavbar'>
+<span class = 'icon-bar'></span>
+<span class = 'icon-bar'></span>
+<span class = 'icon-bar'></span>
+</button>
+<a class = 'navbar-brand' href = '#'> Admin
+</a>
+</div>
+<div class = 'collapse navbar-collapse' id = 'myNavbar'>
+<ul class = 'nav navbar-nav '>
+
+<li><a href = '#'> <i class="glyphicon glyphicon-home"></i>Home</a></li>
+<li><a href = '#'><i class="glyphicon glyphicon-briefcase"></i>profile</a></li>
+<li ><a href = '#'><i class="glyphicon glyphicon-folder-open"></i>Student</a></li>
+
+<!-- drop down -->
+<li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+         aria-expanded="false">
+         <i class="glyphicon glyphicon-question-sign"></i>  Question<i class="glyphicon glyphicon-chevron-down"></i>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#"><i class="glyphicon glyphicon-plus"></i>Add </a>
+</br>
+          <a class="dropdown-item" href="#"><i class="glyphicon glyphicon-trash"></i>Delete </a>
+        
+        </div>
+      </li>
+
+<!--  -->
+<li><a href = '#'><i class="glyphicon glyphicon-list-alt"></i>Exam</a></li>
+<li ><a href = '#'> <i class="glyphicon glyphicon-education"></i> Rank</a></li>
+
+ <!--  drop down -->
+ <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+         aria-expanded="false"><i class="glyphicon glyphicon-bell"></i>
+          Notice<i class="glyphicon glyphicon-chevron-down"></i>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#"> <i class="glyphicon glyphicon-plus"></i>Add </a>
+</br>
+          <a class="dropdown-item" href="#"> <i class="glyphicon glyphicon-trash"></i>Delete </a>
+        
+        </div>
+      </li>
+
+ <!--  -->
+<li ><a href = '#'> <i class="glyphicon glyphicon-envelope"></i>Message</a></li>
+<!-- drop down -->
+
+
+
+<!--  -->
+</ul>
+
+
+<ul class=" nav navbar-nav navbar-right">
+
+<li class="nav-item dropdown " >
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+         aria-expanded="false"><i class="glyphicon glyphicon-user"></i>
+          Account<i class="glyphicon glyphicon-chevron-down"></i>
+
+          
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#"><i class="glyphicon glyphicon-lock"></i>change  </a>
+</br>
+          <a class="dropdown-item" href="#"><i class="glyphicon glyphicon-log-out"></i>Logout </a>
+        
+        </div>
+      </li>
+
+</ul>
+
+</div>
+</div>
+</nav>
+<!--  -->
+
+
+
+
+
+<div class="container-fluid">
+
+<div class="row">
+  <div class="col-md-12">
+
+
+  
+  </div>
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+ <!--  -->
+<script src="js/bootstrap.min.js"  type="text/javascript"></script>
+</body>
+</html>
