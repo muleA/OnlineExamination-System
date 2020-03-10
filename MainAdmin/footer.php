90897c630921f13ef1716c62877a59ad675c60a4
Mulugeta Adamu <56386485+muleA@users.noreply.github.com>
2020-03-10 11:09:37 +0300

    Create footer.php

---
+++ MainAdmin/footer.php
@@ -0,0 +1,16 @@
+<footer class="container-fluid text-center">
+        <div class="row">
+            <div class="col-md-12" style="text-align:center">
+        
+
+            &copy; <script>
+                        var date = document.write(new Date().getFullYear());
+
+                        document.getElementById("year").innerHTML = date;
+                      
+</script>  Online Exam System
+               
+            </div>
+        </div>
+    </footer>
+
